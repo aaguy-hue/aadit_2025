587f8b6c4b037c68d505aec97ff04e3a07c82716
aaguy-hue <aaditmathur6@gmail.com>
2024-10-03 12:38:43 -0700

    finished unit 5 hacks

---
+++ _notebooks/CSA/units_1_to_10/unit_05/Period3/Monkey.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Monkey {
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList();
+        arr.add("hi");
+        arr.add(53);
+
+        System.out.println(arr);
+    }
+}
